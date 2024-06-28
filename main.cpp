33746a8f43e07385faa65100ef366b436c8cb823
bzs <me@bzs.rocks>
2024-06-28 16:28:56 +0200

    add valarray version of the test

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 #include <Eigen/Dense>
 #include <algorithm>
+#include <cassert>
+#include <cstdio>
 #include <iostream>
 #include <random>
+#include <valarray>
 #include <vector>
 
 /*
@@ -32,25 +35,93 @@ std::vector<double> rand_vec(size_t cnt) {
   return ret;
 }
 
-void vector_add(
-    const std::vector<double>& a,
-    const std::vector<double>& b,
-    std::vector<double>& c) {
-  std::transform(a.begin(), a.end(), b.begin(), c.begin(), std::plus<>());
-}
-
-static void BM_VectorAdd(
-    benchmark::State& state,
-    const std::vector<double>& a,
-    const std::vector<double>& b,
-    std::vector<double>& c) {
+static void VectorAdd_RawLoop(benchmark::State& state) {
+  const auto a = rand_vec(num_elements);
+  const auto b = rand_vec(num_elements);
+  auto c = std::vector<double>(num_elements);
   for (auto _ : state) {
-    vector_add(a, b, c);
+    for (int x = 0; x < num_elements; ++x) {
+      c[x] = a[x] + b[x];
+    }
+  }
+  for (auto& x : c) {
+    x *= 2;
+    (void)x;
   }
 }
+BENCHMARK(VectorAdd_RawLoop);
 
-static auto a = rand_vec(num_elements);
-static auto b = rand_vec(num_elements);
-static auto c = std::vector<double>(num_elements);
-BENCHMARK_CAPTURE(BM_VectorAdd, raw_loop, a, b, c);
+static void VectorAdd_StlVec(benchmark::State& state) {
+  const auto a = rand_vec(num_elements);
+  const auto b = rand_vec(num_elements);
+  auto c = std::vector<double>(num_elements);
+  for (auto _ : state) {
+    std::transform(a.cbegin(), a.cend(), b.cbegin(), c.begin(), std::plus<>());
+  }
+}
+BENCHMARK(VectorAdd_StlVec);
+
+struct VectorAdd : public ::benchmark::Fixture {
+  Eigen::VectorXd v1{num_elements};
+  Eigen::VectorXd v2{num_elements};
+  Eigen::VectorXd v3{num_elements};
+  std::valarray<double> val_arr1; // can't use {} due to BENCHMARK macro
+  std::valarray<double> val_arr2;
+  std::valarray<double> val_arr3;
+  void SetUp(::benchmark::State&) {
+    v1.setRandom();
+    v2.setRandom();
+    assert(v1.size() == num_elements);
+    assert(v1.size() == v2.size());
+    assert(v2.size() == v3.size());
+    val_arr1.resize(num_elements);
+    val_arr2.resize(num_elements);
+    val_arr3.resize(num_elements);
+    assert(v2.size() == val_arr1.size());
+    std::copy(v1.begin(), v1.end(), std::begin(val_arr1));
+    std::copy(v2.begin(), v2.end(), std::begin(val_arr2));
+  }
+  void TearDown(::benchmark::State&) {}
+};
+
+BENCHMARK_DEFINE_F(VectorAdd, Valarray)(benchmark::State& st) {
+  // this is reasonably fast, often a tiny bit faster than the raw loop
+  for (auto _ : st) {
+    val_arr3 = val_arr1 + val_arr2;
+  }
+  for (auto& x : val_arr3) {
+    x *= 2;
+    (void)x;
+  }
+}
+BENCHMARK_REGISTER_F(VectorAdd, Valarray);
+
+BENCHMARK_DEFINE_F(VectorAdd, EigenAdd)(benchmark::State& st) {
+  for (auto _ : st) {
+    v3 = v2 + v1;
+  }
+}
+BENCHMARK_REGISTER_F(VectorAdd, EigenAdd);
+
+BENCHMARK_DEFINE_F(VectorAdd, RawLoopAdd)(benchmark::State& st) {
+  // this measures mainly the efficiency of operator[] on Eigen Matrix
+  // the Eigen operator[] probably does more complex things than
+  // std::vector's operator[] and as expected , this performs miserably
+  for (auto _ : st) {
+    for (auto x = 0; x < v1.size(); ++x) {
+      v3[x] = v1[x] + v2[x];
+    }
+  }
+}
+BENCHMARK_REGISTER_F(VectorAdd, RawLoopAdd);
+
+BENCHMARK_DEFINE_F(VectorAdd, StlAdd)(benchmark::State& st) {
+  // runs at about the same speed as EigenAdd
+  // we are probably lucky here because std::plus<>() resolved to
+  // Eigen's efficient version of add
+  for (auto _ : st) {
+    std::transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), std::plus<>());
+  }
+}
+BENCHMARK_REGISTER_F(VectorAdd, StlAdd);
 BENCHMARK_MAIN();
