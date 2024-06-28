e85feffc9f3be60599b1386d275131541ee53e1a
bzs <me@bzs.rocks>
2024-06-28 23:48:42 +0200

    test_mlx: parameterize size of array

--- test_mlx.cpp
+++ test_mlx.cpp
@@ -4,15 +4,16 @@
 
 using namespace mlx::core;
 
+inline constexpr size_t numElements = 1 << 28;
 struct MlxVector : public ::benchmark::Fixture {
   array v1{0.0, complex64};
   array v2{0.0, complex64};
   array v3{0.0, complex64};
   void SetUp(::benchmark::State&) {
-    v1 = random::normal({128 * 1024 * 1024});
-    v2 = random::normal({128 * 1024 * 1024});
-    v3 = random::normal({128 * 1024 * 1024});
-    assert(v1.size() == 128 * 1024 * 1024);
+    v1 = random::normal({numElements});
+    v2 = random::normal({numElements});
+    v3 = random::normal({numElements});
+    assert(v1.size() == numElements);
     assert(v2.size() == v1.size());
     eval(v1);
     eval(v2);
