a0a18072fa027a765f4363455980a67638b46baa
bzs <me@bzs.rocks>
2024-07-11 11:05:28 +0200

    fix test build
    - test_eigen wasn't actually included; building benchmarks as separate
      static lib that the main benchmark executable then links to didn't
      actually link the benchmarks defined in the lib; have to include
      source in the exe

--- tests/test_openmp.cpp
+++ tests/test_openmp.cpp
@@ -4,16 +4,17 @@
 #include <vector>
 #include "config.h"
 
-static void VectorAdd_StlVec_OpenMP(benchmark::State& state) {
+static void DISABLED_VectorAdd_StlVec_OpenMP(benchmark::State& state) {
   auto a = std::vector<double>(numElements);
   auto b = std::vector<double>(numElements);
   auto c = std::vector<double>(numElements);
-#pragma omp parallel
+
   for (auto _ : state) {
+    #pragma omp parallel
 #pragma omp for
     for (auto x = 0; x < a.size(); ++x) {
       c[x] = a[x] + b[x];
     }
   }
 }
-// BENCHMARK(VectorAdd_StlVec_OpenMP);  // disable until #2 is fixed
+BENCHMARK(DISABLED_VectorAdd_StlVec_OpenMP); // Disable until #3 fixed
